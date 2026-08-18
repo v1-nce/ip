90109ca470369e028c4f7ea0b198690faccfc17c
v1-nce <vincentongkaisen6@gmail.com>
2026-08-18 19:45:47 +0800

    Change Duke to Summer

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Summer User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Summer.java
@@ -0,0 +1,10 @@
+public class Summer {
+    public static void main(String[] args) {
+        String banner = "                                           \n"
+                + " ___ _   _ _ __ ___  _ __ ___   ___ _ __ \n"
+                + "/ __| | | | '_ ` _ \\| '_ ` _ \\ / _ \\ '__|\n"
+                + "\\__ \\ |_| | | | | | | | | | | |  __/ |   \n"
+                + "|___/\\__,_|_| |_| |_|_| |_| |_|\\___|_|   \n";
+        System.out.println(banner);
+    }
+}
