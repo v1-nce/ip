4ff0c2fc91396dda31281afcf46543a64189f334
v1-nce <vincentongkaisen6@gmail.com>
2026-08-18 19:53:38 +0800

    Implement Summer Greeting

--- src/main/java/Summer.java
+++ src/main/java/Summer.java
@@ -1,10 +1,18 @@
 public class Summer {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = "                                           \n"
                 + " ___ _   _ _ __ ___  _ __ ___   ___ _ __ \n"
                 + "/ __| | | | '_ ` _ \\| '_ ` _ \\ / _ \\ '__|\n"
                 + "\\__ \\ |_| | | | | | | | | | | |  __/ |   \n"
                 + "|___/\\__,_|_| |_| |_|_| |_| |_|\\___|_|   \n";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Summer.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
